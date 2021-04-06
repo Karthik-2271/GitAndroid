82a63ac29b2e94593663150d688e7891b7c7e2eb
karthik.u <karthikudupa1998@gmail.com>
2021-04-06 12:10:21 +0530

    commit from android studio

--- app/src/main/java/com/example/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/gitandroid/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("GIT","Commit here");
+        Log.i("GIT","Commit here");
     }
 }
